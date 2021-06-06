ada77e035319c92131d34e5148c5ead4242424de
Modu <35815941+hmodu@users.noreply.github.com>
2021-06-06 07:53:31 +0100

    Add products.js

---
+++ products.js
@@ -0,0 +1,82 @@
+let lessons = [
+    {
+    "id":101 ,
+    "subject": "Art",
+    "location": "Abuja",
+    "price": 500,
+    "availableInventory":5,
+    "image": "images/art.jpg"
+},
+{
+    "id":102 ,
+    "subject": "Math",
+    "location": "Lagos",
+    "price": 550,
+    "availableInventory":5,
+    "image": "images/Math.JPEG"
+},
+{
+    "id":103 ,
+    "subject": "ICT",
+    "location": "Ilorin",
+    "price": 650,
+    "availableInventory":5,
+    "image": "images/ict.jpg"
+},
+{
+    "id":104 ,
+    "subject": "English",
+    "location": "Abuja",
+    "price": 350,
+    "availableInventory":5,
+    "image": "images/english.jpg"
+},
+{
+    "id":105 ,
+    "subject": "French",
+    "location": "Lagos",
+    "price": 350,
+    "availableInventory":5,
+    "image": "images/french.jpg"
+},
+{
+    "id":106 ,
+    "subject": "Geography",
+    "location": "Abuja",
+    "price": 450,
+    "availableInventory":5,
+    "image": "images/geo.jpg"
+},
+{
+    "id":107 ,
+    "subject": "Literature",
+    "location": "Lagos Island",
+    "price": 400,
+    "availableInventory":5,
+    "image": "images/lit.jpg"
+},
+{
+    "id":108 ,
+    "subject": "Physics",
+    "location": "Abuja",
+    "price": 450,
+    "availableInventory":5,
+    "image": "images/physics.jpg"
+},
+{
+    "id":109 ,
+    "subject": "Chemistry",
+    "location": "Ilorin",
+    "price": 550,
+    "availableInventory":5,
+    "image": "images/chem.jpg"
+},
+{
+    "id":110 ,
+    "subject": "Music",
+    "location": "Abuja",
+    "price": 300,
+    "availableInventory":5,
+    "image": "images/music.jpg"
+},
+]
